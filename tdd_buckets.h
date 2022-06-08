7fcf8f9cd0ad9f58430eb99e950b99365c89bd85
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-06-09 01:28:38 +0530

    Add files via upload

--- tdd_buckets.h
+++ tdd_buckets.h
@@ -5,33 +5,34 @@
 using namespace std;
 
 vector<int> values = {3, 3, 4, 5, 10, 11, 12};
-//values.assign({3, 3, 5, 4, 10, 11, 12});
+
 int count1 = 0;
 bool flag = false;
 
-void checkAndStartCount(){
+void checkAndCountUp(){
     if (flag){
         count1++;
     }
 }
-void updateCount(int value,int &a, int &b){
-    if (value==a){
-        flag = true;
-    }
+void checkAndStopCount(int value,int &b){
     if(value==b){
         count1++;
         flag = false;
     }
-    checkAndStartCount();
+}
+void checkAndCount(int value,int &a, int &b){
+    if (value==a){
+        flag = true;
+    }
+    checkAndStopCount(value,b);
+    checkAndCountUp();
 }
 
 int getReadingsFromRange(int a ,int b){
     count1 = 0;
     for (long long unsigned int i = 0; i<values.size();i++){
-        cout<<values[i]<<endl;
-        updateCount(values[i],a,b);
+        checkAndCount(values[i],a,b);
     }
-    cout<<endl;
-    cout<<count1<<endl;
+    cout<<"Count = "<<count1<<endl;
     return count1;
 }
