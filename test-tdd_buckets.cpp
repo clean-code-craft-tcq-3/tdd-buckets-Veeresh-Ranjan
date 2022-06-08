be39eb49a9c07730638cc6dd4f060f3b9bb2ddec
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-06-09 00:48:59 +0530

    Add files via upload

--- test-tdd_buckets.cpp
+++ test-tdd_buckets.cpp
@@ -6,32 +6,36 @@ using namespace std;
 
 vector<int> values = {3, 3, 4, 5, 10, 11, 12};
 //values.assign({3, 3, 5, 4, 10, 11, 12});
+int count1 = 0;
+bool flag = false;
 
+updateCount(int value,int &a, int &b){
+    if (value==a){
+        flag = true;
+    }
+    if(value==b){
+        count1++;
+        flag = false;
+    }
+    if (flag){
+        count1++;
+    }
+}
 
 int getReadingsFromRange(int a ,int b){
-    int count = 0;
-    bool flag = 0;
+    count1 = 0;
     for (long long unsigned int i = 0; i<values.size();i++){
         cout<<values[i]<<endl;
-        if (values[i]==a){
-            flag = true;
-        }
-        if(values[i]==b){
-            count++;
-            flag = false;
-            break;
-        }
-        if (flag){
-            count++;
-        }
+        updateCount(values[i],a,b);
     }
     cout<<endl;
-    cout<<count<<endl;
-    return count;
+    cout<<count1<<endl;
+    return count1;
 }
 
 int main(){
     assert(getReadingsFromRange(3,5)==4);
+    assert(getReadingsFromRange(4,5)==2);
     assert(getReadingsFromRange(3,4)==3);
     assert(getReadingsFromRange(10,12)==3);
     assert(getReadingsFromRange(3,12)==7);
