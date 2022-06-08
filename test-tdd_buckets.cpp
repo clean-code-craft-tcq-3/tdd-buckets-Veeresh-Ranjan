2c72881d1ff49c7fe7bf13db876af0e6749f3077
Veeresh-Ranjan <99407299+Veeresh-Ranjan@users.noreply.github.com>
2022-06-09 01:17:49 +0530

    Add files via upload

--- test-tdd_buckets.cpp
+++ test-tdd_buckets.cpp
@@ -1,40 +1,4 @@
-#include "iostream"
-#include "assert.h"
-#include <bits/stdc++.h>
-#include <vector>
-using namespace std;
-
-vector<int> values = {3, 3, 4, 5, 10, 11, 12};
-//values.assign({3, 3, 5, 4, 10, 11, 12});
-int count1 = 0;
-bool flag = false;
-
-void checkAndStartCount(){
-    if (flag){
-        count1++;
-    }
-}
-void updateCount(int value,int &a, int &b){
-    if (value==a){
-        flag = true;
-    }
-    if(value==b){
-        count1++;
-        flag = false;
-    }
-    checkAndStartCount();
-}
-
-int getReadingsFromRange(int a ,int b){
-    count1 = 0;
-    for (long long unsigned int i = 0; i<values.size();i++){
-        cout<<values[i]<<endl;
-        updateCount(values[i],a,b);
-    }
-    cout<<endl;
-    cout<<count1<<endl;
-    return count1;
-}
+#include "tdd_buckets.h"
 
 int main(){
     assert(getReadingsFromRange(3,5)==4);
